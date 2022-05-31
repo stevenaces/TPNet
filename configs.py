2971b7e92b5bc5534f3c0b859dee72ac51917774
stevenaces <3313899200@qq.com>
2022-05-31 22:08:32 +0800

    update: 修改miniImageDate数据集位置配置

--- configs.py
+++ configs.py
@@ -1,5 +1,6 @@
 save_dir                    = ''
 data_dir = {}
-data_dir['CUB']             = '/data2/lrmshuju/datasets/CUB/'
-data_dir['miniImagenet']    = ''
+# data_dir['CUB']             = '/data2/lrmshuju/datasets/CUB/'
+data_dir['CUB']             = ''
+data_dir['miniImagenet']    = '/data/nfs/datasets/xiesjok/miniImageDataset'
 
